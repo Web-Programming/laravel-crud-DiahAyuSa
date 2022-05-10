9239275b87cc23bde673ccbf884921d386a49325
DiahAyuSa <ayusdiah39@gmail.com>
2022-05-10 12:28:51 +0700

    resource view Index.blade.php

--- resources/views/prodi/index.blade.php
+++ resources/views/prodi/index.blade.php
@@ -1,17 +1,30 @@
 {{-- @include("layout.header", ['title' => "Halaman Program Studi"]) --}}
 
 @extends("layout.master")
-@section("title", "Halaman Program Studi")
+@section("title", "Halaman List Prodi")
 
 @section("content")
-    <h1>Program Studi</h1>
-    @if(count($prodi) > 0 )
-        @foreach($prodi as $item)
-            <li>{{ $item->nama }} {{ $item->fakultas->nama }}</li>
-        @endforeach
-    @else
-        Program Studi tidak ditemukan
-    @endif
+<div class="row pt-4">
+    <div class="col">
+        <h2>Prodi</h2>
+        <div class="d-md-flex justify-content-md-end">
+            <a href="{{ route('prodi.create') }}" class="btn btn-primary">Tambah</a>
+</div>
+<table class="table table-striped table-hover">
+    <head>
+        <tr>
+            <th>Nama</th><th>Aksi</th>
+</tr>
+</thead>
+<tbody>
+    @foreach ($prodi as $item)
+    <tr>
+        <td> {{ $item->nama }} </td>
+        <td><a href="{{ url('/prodi/'.$item->id) }}" class="btn btn-warning">Detail</a></td>
+</tr>
+@endforeach
+</tbody>
+</table>
+</div>
+</div>
 @endsection
-
-{{-- @include("layout.footer") --}}

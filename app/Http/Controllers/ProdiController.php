9ce8c2f4f5c11d30799b2f7c43e42473ccfdc082
DiahAyuSa <ayusdiah39@gmail.com>
2022-05-10 15:53:34 +0700

    finish

--- app/Http/Controllers/ProdiController.php
+++ app/Http/Controllers/ProdiController.php
@@ -9,56 +9,63 @@ use Illuminate\Support\Facades\DB;
 class ProdiController extends Controller
 {
 
-    public function index()
-    {
+    function index(){
+    
+        $kampus = "Universitas Multi Data Palembang";
         $prodi = Prodi::all();
         return view('prodi.index')->with('prodi', $prodi);
     }
-        /*$data = [
-            'prodi' => ['Manajemen Informatika', 'Sistem Informasi', 'Informatika']
-        ];
-    
-        //atau menggunakan compact
-        $prodi = ['Manajemen Informatika', 'Sistem Informasi', 'Informatika'];
-        $kampus = "Universitas Multi Data Palembang";
-    
-        return view("prodi.index", compact('prodi', 'kampus'));*/
-
-     /*   $kampus = "Universitas Multi Data Palembang";
-
-        /*$prodi = DB::select("SELECT prodi.*, fakultas.nama as namaf FROM prodi INNER JOIN fakultas ON prodi.fakultas_id = fakultas.id");*/
-        
-      /*  return view("prodi.index", compact('kampus', 'prodi'));
-    } */
 
     function detail($id = null){
         echo $id;
     }
 
-    public function create()
-    {
+    function create(){
         return view("prodi.create");
     }
 
     public function store(Request $request)
     {
-       // dump($request);
-       //echo $request->nama;
-      // $request->input('Diah');
-       //request('Diah');
+        $validateData = $request->validate([
+            'nama' => 'required|min:5|max:20',
+        ]);
+       
+        $prodi = new Prodi(); //buat object prodi
+        $prodi->nama = $validateData['nama']; //simpan nilai inout ($validateData['nama]) ke dalam property nama prodi ($prodi->nama)
+        $prodi->institusi_id = 0;
+        $prodi->fakultas_id = 1;
+        $prodi->save(); //simpan ke dalam tabel prodis
 
-       $validateData = $request->validate([
-           'nama' => 'required|min:5|max:20',
-       ]);
-      // dump($validateData);
-      // echo $validateData['nama'];
-
-      $prodi = new Prodi();
-      $prodi->nama = $validateData['nama'];
-      $prodi->save();
-
-      $request->session()->flash('info', "Data Prodi $prodi->nama berhasil disimpan ke database");
-      return redirect()->route('prodi.create');
+        //return "Data prodi $prodi->nama berhasil disimpan ke database"; // tampilkan pesan berhasil
+        $request->session()->flash('info', "Data prodi $prodi->nama berhasil disimpan ke database");
+        return redirect()->route('prodi.create');
     }
 
+    public function show(Prodi $prodi)
+    {
+        return view('prodi.show', ['prodi' => $prodi]);
+    }
+
+    public function edit(Prodi $prodi)
+    {
+        return view('prodi.edit', ['prodi' => $prodi]);
+    }
+
+    public function update(Request $request, Prodi $prodi)
+    {
+     
+        $validateData = $request->validate([
+            'nama' => 'required|min:5|max:20',
+        ]);
+
+        Prodi::where('id', $prodi->id)->update($validateData);
+        $request->session()->flash('info', "Data prodi $prodi->nama berhasil diubah");
+        return redirect()->route('prodi.index');
+    }
+
+    public function destroy(Prodi $prodi)
+    {
+        $prodi->delete();
+        return redirect()->route('prodi.index')->with("info", "Prodi $prodi->nama berhasil dihapus.");
+    }
 }
--- resources/views/prodi/create.blade.php
+++ resources/views/prodi/create.blade.php
@@ -5,33 +5,23 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Form Prodi</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
     <div class="container">
-         <div class="row pt-4">
-             <div class="col">
-                 <h2>Form Prodi</h2>
-                 @if (session()->has('info'))
-                 <div class="alert alert-succes">
-                     {{ session()->get('info') }}
-</div>
-                @endif
-                 <form action="{{url('prodi/store')}}" method="post">
-                     @csrf
-                     <div class="form-group">
-                         <label for="nama">Nama</label>
-                         <input type="text" name="nama" id="nama" class="form-control">
-                         value="{{ old('nama') }}">
-                         @eerror('nama')
-                         <div class="text-danger"> {{ $message }} </div>
-                         @enderror
-
-</div>
-<button type="submit" class="btn btn-primary mt-2">Simpan</button>
-<form>
-</div>
-</div>
-</div>   
+        <div class="row pt-4">
+            <div class="col">
+                <h2>Form Prodi</h2>
+                <form action="{{ url('prodi/store') }}" method="post">
+                    @csrf   
+                    <div class="form-group">
+                        <label for="nama">Nama</label>
+                        <input type="text" name="nama" id="nama" class="form-control">
+                    </div>
+                    <button type="submit" class="btn btn-primary mt-2">Simpan</button>
+                </form>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
